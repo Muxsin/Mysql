1ead1337a270270d2540c90f82dc519ba362820d
Muhsin <muhsin@White.Hat>
2019-08-06 06:02:17 +0700

    Login

---
+++ Complete login system/includes/dbh.inc.php
@@ -0,0 +1,7 @@
+<?php
+    $dbServername = "localhost";
+    $dbUsername = "muhsin";
+    $dbPassword = "Khonabardush1";
+    $dbName = "loginsystem";
+    
+    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
